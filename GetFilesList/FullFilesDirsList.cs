62d8d13b2c852f907ffde4a78e94ff8c8d60e2d3
Василий Артюков <ArtiukovVI@gmail.com>
2023-03-25 00:18:33 +0300

    Added annotations and change logic of folders collection, wich now include start folder

--- GetFilesList/FullFilesDirsList.cs
+++ GetFilesList/FullFilesDirsList.cs
@@ -1,26 +1,72 @@
 ﻿
 
+using System.Data;
+
 namespace GetFilesList
 {
     public class FullFilesDirsList
     {
+        /// <summary> 
+        /// Get the <see cref="List"/> of <see cref="FileInfo"/> that belong to this instance.
+        /// </summary>
         public List<FileInfo> Files { get; private set; } = new List<FileInfo>();
+
+        /// <summary> 
+        /// Get the <see cref="List"/> of <see cref="DirectoryInfo"/> that belong to this instance.
+        /// </summary>
+
         public List<DirectoryInfo> Folders { get; private set; } = new List<DirectoryInfo>();
+        
+        /// <summary> 
+        /// Get the List of tuples which consisting of <see cref="DirectoryInfo"/> and <see cref="Exception"/> that occurred when trying to access it.
+        /// </summary>
         public List<(DirectoryInfo, Exception)> NotAccessedFolders { get; private set; } = new List<(DirectoryInfo, Exception)>();
 
-        public static FullFilesDirsList Clone()
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FullFilesDirsList"/> class.
+        /// </summary>
+        public FullFilesDirsList()
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FullFilesDirsList"/> class.
+        /// </summary>
+        public FullFilesDirsList Clone()
         {
             return new FullFilesDirsList();
         }
 
+        /// <summary>
+        /// Clear all properties of current instance.
+        /// </summary>
+        public void Clear()
+        {
+            Files.Clear();
+            Folders.Clear();
+            NotAccessedFolders.Clear();
+        }
+
+        /// <summary>
+        /// Recursively walk through all folders and fill properties.
+        /// </summary>
+        /// <param name="dirPath">Directory from which files and folders are taken.</param>
+        /// <param name="searchPattern">The search string to match against the names of files.</param>
         public void GetFilesDirsList(string dirPath, string searchPattern)
         {
             DirectoryInfo dirInfo = new(dirPath);
             GetFilesDirsList(dirInfo, searchPattern);
         }
 
+        /// <summary>
+        /// Recursively walk through all folders and fill properties.
+        /// </summary>
+        /// <param name="dirInfo">Directory from which files and folders are taken.</param>
+        /// <param name="searchPattern">The search string to match against the names of files.</param>
         public void GetFilesDirsList(DirectoryInfo dirInfo, string searchPattern)
         {
+            Folders.Add(dirInfo);
             try
             {
                 foreach (FileInfo f in dirInfo.GetFiles(searchPattern))
@@ -37,16 +83,32 @@ namespace GetFilesList
 
             foreach (DirectoryInfo d in dirInfo.GetDirectories())
             {
-                Folders.Add(d);
                 GetFilesDirsList(d, searchPattern);
             }
         }
 
+        /// <summary>
+        /// Recursively in parallel walk through all folders.
+        /// </summary>
+        /// <param name="dirPath">Directory from which files and folders are taken.</param>
+        /// <param name="searchPattern">The search string to match against the names of files.</param>
+        /// <returns>Returns a new instance of the <see cref="FullFilesDirsList"/> with filled properties.</returns>
+        public static FullFilesDirsList GetFilesDirsListParallel(string dirPath, string searchPattern)
+        {
+            DirectoryInfo dirInfo = new(dirPath);
+            return GetFilesDirsListParallel(dirInfo, searchPattern);
+        }
+
+        /// <summary>
+        /// Recursively in parallel walk through all folders.
+        /// </summary>
+        /// <param name="dirInfo">Directory from which files and folders are taken.</param>
+        /// <param name="searchPattern">The search string to match against the names of files.</param>
+        /// <returns>Returns a new instance of the <see cref="FullFilesDirsList"/> with filled properties.</returns>
         public static FullFilesDirsList GetFilesDirsListParallel(DirectoryInfo dirInfo, string searchPattern)
         {
             FullFilesDirsList result = new();
             DirectoryInfo[] subDirs;
-
             try
             {
                 subDirs = dirInfo.GetDirectories();
@@ -54,6 +116,7 @@ namespace GetFilesList
             catch (Exception ex)
             {
                 Console.WriteLine("Directory {0}  \n could not be accessed!!!!", dirInfo.FullName);
+                result.Folders.Add(dirInfo);
                 result.NotAccessedFolders.Add((dirInfo, ex));
                 return result;
             }
@@ -82,7 +145,7 @@ namespace GetFilesList
 
             result = SelectMany(allTasks.Select(t => t.Result));
 
-            result.Folders.AddRange(subDirs);
+            result.Folders.Add(dirInfo);
             result.Files.AddRange(dirInfo.GetFiles(searchPattern));
 
             return result;
